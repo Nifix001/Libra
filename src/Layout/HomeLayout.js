c0f547c25e4a1773524bc92e3fb009697fd2de30
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-02-28 22:34:14 +0100

    New commit

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -1,18 +1,27 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import rec3 from "../images/Rectangle 3.png"
+import vector from "../images/chevron-down.png"
 
 export default function HomeLayout() {
   return (
     <div className="home-layout">
+      <div className="home-layout1">
       <div>
       <h1>Studying books, <br /> a long lasting way <br /> to changing lives</h1>
-      <p>We relieve you a great deal by source <br /> books for you to lend, read, and change <br /> your lives. We are always ready to help</p>
+      <p>We relieve you a great deal by sourcing <br /> books for you to lend, read, and change <br /> your lives. We are always ready to help</p>
       <div className="nvl">
       <NavLink to="Learn" >Learn More</NavLink>
       <NavLink to="contact">Contact Us</NavLink>
       </div>
       </div>
       <img src={rec3} alt="reading girl" />
+      </div>
+      <div className="arrow">
+        <NavLink to="services" ><img src={vector} alt="arrow" /></NavLink>
+        </div>
+
+
+      <Outlet />
     </div>
   )
 }
