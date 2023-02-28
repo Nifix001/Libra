17100fa71d07909c3d59691d5429651cde666326
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-02-28 19:25:19 +0100

    New commit

--- src/Layout/HomeLayout.js
+++ src/Layout/HomeLayout.js
@@ -6,7 +6,7 @@ export default function HomeLayout() {
     <div className="home-layout">
       <div>
       <h1>Studying books, <br /> a long lasting way <br /> to changing lives</h1>
-      <p>We relieve you a great deal by sourcing <br /> books for you to lend, read, and change <br /> your lives. We are always ready to help</p>
+      <p>We relieve you a great deal by source <br /> books for you to lend, read, and change <br /> your lives. We are always ready to help</p>
       <div className="nvl">
       <NavLink to="Learn" >Learn More</NavLink>
       <NavLink to="contact">Contact Us</NavLink>
