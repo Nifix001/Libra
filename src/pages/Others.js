7637f967a8c40f2c3df481b9c3ab7c900826269c
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-02 22:32:28 +0100

    New commit

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -1,63 +1,90 @@
 import { NavLink, Outlet } from "react-router-dom";
-import vector from "../images/chevron-down.png"
+import vector from "../images/chevron-down.png";
+import ot1 from "../images/image 16.png";
+import ot2 from "../images/image 14.png";
+import ot3 from "../images/image 13.png";
+import ot4 from "../images/image 12.png";
+import ot5 from "../images/Ellipse 1.png";
+import ot6 from "../images/Ellipse 2.png";
+import ot7 from "../images/Ellipse 3.png";
+import ot8 from "../images/Ellipse 4.png";
 
 export default function Others() {
   return (
     <div>
         <div className="others">
-            <h2>Other Books</h2>
-            <div>
-                <div>
+            <h3>Other Books</h3>
+            <div className="others-1">
+                <div className="others-2">
                     <NavLink to="/">All</NavLink>
                     <select>
                         <option value="">Genre</option>
                     </select>
                 </div>
+                <div>
                 <input type="text" placeholder="Search by name" />
+                <i></i>
+                </div>
             </div>
-            <div>
-                <div className="books">
-                    <img src="" alt="books" />
-                    <h4>The Alchemist</h4>
+            <div className="trend">
+            <div className="trends-1">
+                <div className="trends-2">
+                    <img src={ot1} alt="" />
+                </div>
+                <div className="trends-3">
+                    <h5>The Alchemist</h5>
                     <h6>Paulo Coelho</h6>
-                    <div>
+                    <div className="trends-4">
                         <i></i>
                         <button>View details</button>
                     </div>
                 </div>
-                <div className="books">
-                    <img src="" alt="books" />
-                    <h4>A confederacy of dunces</h4>
-                    <h6>John Kennedy T.</h6>
-                    <div>
+            </div>
+            <div className="trends-1">
+                <div className="trends-2">
+                    <img src={ot2} alt="" />
+                </div>
+                <div className="trends-3">
+                    <h5>A confederacy of dunces</h5>
+                    <h6>John Kenneddy T.</h6>
+                    <div className="trends-4">
                         <i></i>
                         <button>View details</button>
                     </div>
                 </div>
-                <div className="books">
-                    <img src="" alt="books" />
-                    <h4>Why we sleep</h4>
+            </div>
+            <div className="trends-1">
+                <div className="trends-2">
+                    <img src={ot3} alt="" />
+                </div>
+                <div className="trends-3">
+                    <h5>Why we sleep</h5>
                     <h6>Matthew Walker</h6>
-                    <div>
+                    <div className="trends-4">
                         <i></i>
                         <button>View details</button>
                     </div>
                 </div>
-                <div className="books">
-                    <img src="" alt="books" />
-                    <h4>An enemy called average</h4>
-                    <h6>John L.Mason</h6>
-                    <div>
+            </div>
+            <div className="trends-1">
+                <div className="trends-2">
+                    <img src={ot4} alt="" />
+                </div>
+                <div className="trends-3">
+                    <h5>An enemy called average</h5>
+                    <h6>John L. Mason</h6>
+                    <div className="trends-4">
                         <i></i>
                         <button>View details</button>
                     </div>
-                    <div className="nav-btn">
-                        <NavLink><div></div></NavLink>
-                        <NavLink><div></div></NavLink>
-                        <NavLink><div></div></NavLink>
-                        <NavLink><div></div></NavLink>
-                    </div>
                 </div>
+            </div>
+            </div>
+            <div className="nav-btn">
+            <NavLink><div></div></NavLink>
+            <NavLink><div></div></NavLink>
+            <NavLink><div></div></NavLink>
+            <NavLink><div></div></NavLink>
             </div>
       </div>
       <div className="reviews">
@@ -65,7 +92,7 @@ export default function Others() {
             <div>
                 <div className="personal-reviews">
                     <div className="personal-details">
-                        <img src="" alt="" />
+                        <img src={ot5} alt="" />
                         <div>
                             <h4>Karoline Red</h4>
                             <h5>Chef</h5>
@@ -76,7 +103,7 @@ export default function Others() {
                 </div>
                 <div className="personal-reviews">
                     <div className="personal-details">
-                        <img src="" alt="" />
+                        <img src={ot6} alt="" />
                         <div>
                             <h4>Collins Aloba</h4>
                             <h5>Teacher</h5>
@@ -87,7 +114,7 @@ export default function Others() {
                 </div>
                 <div className="personal-reviews">
                     <div className="personal-details">
-                        <img src="" alt="" />
+                        <img src={ot7} alt="" />
                         <div>
                             <h4>Alo Bosede</h4>
                             <h5>Forex Trader</h5>
@@ -98,7 +125,7 @@ export default function Others() {
                 </div>
                 <div className="personal-reviews">
                     <div className="personal-details">
-                        <img src="" alt="" />
+                        <img src={ot8} alt="" />
                         <div>
                             <h4>Bill Brown</h4>
                             <h5>Sales Manager</h5>
