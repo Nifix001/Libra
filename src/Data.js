66c6204e59dfac36bf914fbeb2c300b0cc29d9ba
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-07 06:29:39 +0100

    New commit

--- src/Data.js
+++ src/Data.js
@@ -1,6 +1,3 @@
-import img1 from "../images/🦆 illustration _Vision books telescope_.png";
-import img2 from "../images/🦆 icon _apple books_.png";
-import img3 from "../images/🦆 illustration _Logistics_.png";
 import img4 from "../images/image 7.png";
 import img5 from "../images/image 9.png";
 import img6 from "../images/image 11.png";
@@ -11,56 +8,28 @@ export default [
         id: 1,
         title: "Steal like an artist",
         author: "Austin Klean",
-        image: img1,
+        image: img4,
         button: "View details"
     },
     {
         id: 2,
         title: "Half of a yellow sun",
         author: "Austin Klean",
-        image: img2,
+        image: img5,
         button: "View details"
     },
     {
         id: 3,
         title: "The sun and her flowers",
         author: "Rupi Kour",
-        image: img3,
+        image: img6,
         button: "View details"
     },
     {
         id: 4,
         title: "Purple Hisbiscus",
         author: "Chimamanda Adichie",
-        image: img4,
-        button: "View details"
-    },
-    {
-        id: 5,
-        title: "The Alchemist",
-        author: "Paulo Coelho",
-        image: img5,
-        button: "View details"
-    },
-    {
-        id: 6,
-        title: "A confederacy of dunces",
-        author: "John Kennedy T.",
-        image: img6,
-        button: "View details"
-    },
-    {
-        id: 7,
-        title: "Why we sleep",
-        author: "Matthew Walker",
         image: img7,
-        button: "View details"
-    },
-    {
-        id: 8,
-        title: "An enemycalled average",
-        author: "John L.Mason",
-        image: img8,
         button: "View details"
     },
 ]
