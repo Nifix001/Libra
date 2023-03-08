1883d443479c8258fbf5e6bd0ed3e61dde4194e6
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-03-08 18:29:01 +0100

    New commit

--- src/database/Data.js
+++ src/database/Data.js
@@ -1,4 +1,3 @@
-import img4 from "../images/image 7.png";
 import img5 from "../images/image 9.png";
 import img6 from "../images/image 11.png";
 import img7 from "../images/image 8.png";
@@ -7,27 +6,20 @@ import img7 from "../images/image 8.png";
 const data = [
     {
         id: 1,
-        title: "Steal like an artist",
-        author: "Austin Klean",
-        image: img4,
-        button: "View details"
-    },
-    {
-        id: 2,
         title: "Half of a yellow sun",
         author: "Austin Klean",
         image: img5,
         button: "View details"
     },
     {
-        id: 3,
+        id: 2,
         title: "The sun and her flowers",
         author: "Rupi Kour",
         image: img6,
         button: "View details"
     },
     {
-        id: 4,
+        id: 3,
         title: "Purple Hisbiscus",
         author: "Chimamanda Adichie",
         image: img7,
