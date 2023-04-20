cdb40b5085e3420b7b39b411b6d1b8b68ab4979f
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-04-20 22:02:42 +0100

    New commit

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -7,7 +7,6 @@ import Trending from "../framework/Trending";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/pagination";
-import "./styles.css";
 import { Pagination } from "swiper";
 
 export default function Others() {
