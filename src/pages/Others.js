6fc0edfc1acc6acb527ab1e322c2bdcb970aea75
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-04-21 10:00:07 +0100

    New ccommit

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -57,24 +57,6 @@ export default function Others() {
         pagination={{
           clickable: true,
         }}
-        breakpoints={{
-          "@0.00": {
-            slidesPerView: 1,
-            spaceBetween: 10,
-          },
-          "@0.75": {
-            slidesPerView: 2,
-            spaceBetween: 20,
-          },
-          "@1.00": {
-            slidesPerView: 3,
-            spaceBetween: 40,
-          },
-          "@1.50": {
-            slidesPerView: 4,
-            spaceBetween: 50,
-          },
-        }}
         modules={[Pagination]}
         className="mySwiper"
       >
