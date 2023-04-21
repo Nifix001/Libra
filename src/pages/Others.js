bbfd6e809aa66778d70592443029e506fc98d6d5
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-04-21 09:18:45 +0100

    New commit

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -51,7 +51,35 @@ export default function Others() {
                 </div>
             </div>
             <div className="trend">
-                { dataO }
+            <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          "@0.00": {
+            slidesPerView: 1,
+            spaceBetween: 10,
+          },
+          "@0.75": {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          "@1.00": {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          "@1.50": {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>{ dataO }</SwiperSlide>
+      </Swiper>
             </div>
             <div className="nav-btn">
             <NavLink><div></div></NavLink>
