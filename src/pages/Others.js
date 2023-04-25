55329254e51452daa461b2be55e1b1ffabe9ff44
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-04-25 21:52:34 +0100

    New commit

--- src/pages/Others.js
+++ src/pages/Others.js
@@ -15,18 +15,29 @@ export default function Others() {
     const dataO = DataO.map(others => {
         return(
           <>
-          <Swiper
-          spaceBetween={30}
-          autoplay={{
-            delay: 2500,
-            disableOnInteraction: false,
-          }}
-          pagination={{
-            clickable: true,
-          }}
-          modules={[ Autoplay, Pagination, Navigation]}
-          navigation={true}
-        >
+               <Swiper
+        slidesPerView={1}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        breakpoints={{
+          640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 5,
+            spaceBetween: 50,
+          },
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
             <Trending
             key={others.id}
             image={others.image}
