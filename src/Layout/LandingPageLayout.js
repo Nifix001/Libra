d4b83fa0791889b076d50e26c641d28387e52fbb
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-05-03 21:38:26 +0100

    New commit

--- src/Layout/LandingPageLayout.js
+++ src/Layout/LandingPageLayout.js
@@ -1,12 +1,13 @@
 import { NavLink, Outlet } from "react-router-dom";
 import img1 from "../images/Frame.png";
+import { motion } from "framer-motion"
 
 export default function LandingPageLayout() {
   return (
     <div className="landing">
       <header>
         <img src={img1} alt="logo" />
-        <nav>
+        <motion.nav>
             <NavLink to="/">Home</NavLink>
             <NavLink to="services">Services</NavLink>
             <NavLink to="settings">
@@ -15,7 +16,7 @@ export default function LandingPageLayout() {
                 </select>
                 </NavLink>
             <NavLink to="help">Help</NavLink>
-        </nav>
+        </motion.nav>
         <div className="side-bar">
             <NavLink to="sign-up">Sign Up</NavLink>
             <NavLink to="login" >Login</NavLink>
