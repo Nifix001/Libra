d2878941be158b590c7049019debe7cb00ff8545
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-05-10 21:13:34 +0100

    New commit

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,21 +1,28 @@
 import { Link, NavLink, } from "react-router-dom";
 import fot from "../images/Rectangle 15.png"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { motion } from "framer-motion";
 
 
 export default function Footer() {
   return (
     <div className="footer">
-        <div className="footer-a">
+        <motion.div className="footer-a"
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5, delay: 0.8}}
+        >
            <img src={fot} alt="" /> 
            <div className="footer-b">
-
             <h3>We dedicate time diligently to provide <br /> books for you <hr /></h3>
             <p>Read through our code of conduct and terms of agreement on<br /> borrowing of books</p>
             <button>Read</button>
            </div>
-        </div>
-        <div className="footer-c">
+        </motion.div>
+        <motion.div className="footer-c"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1, delay: 1}}>
             <p>Contact us on:</p>
             <div className="socials">
             <Link to='/'><FontAwesomeIcon icon="fa-brands fa-facebook" style={{color: "white", fontSize:"50px"}} /></Link>
@@ -37,7 +44,7 @@ export default function Footer() {
             <hr />
             </div>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non minus possimus provident!<br /> Quod explicabo inventore in est vero impedit quo doloribus cumque maiores!</p>
-        </div>
+        </motion.div>
         
     </div>
   )
