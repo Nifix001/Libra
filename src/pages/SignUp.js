7cf2340444b9ad02c061f6d72ead0d120430739d
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-05-16 14:31:38 +0100

    New commit

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -44,7 +44,7 @@ export default function SignUp( ) {
         </header>
         <button onClick={toggleModal}>Sign Up</button>
         <div className="modal"></div>
-        <motion.div 
+        <motion.div className="overlay"
         initial={{ opacity: 0,}}
         animate={{ opacity: 1,}}
         >
