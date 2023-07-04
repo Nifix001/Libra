eecf9b34bc024480f74e90a409e963a58f4ad986
Nifix001 <101341614+Nifix001@users.noreply.github.com>
2023-07-04 18:46:56 +0100

    commit

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,4 +1,4 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import vector from "../images/chevron-down.png";
 import data from "../database/Data";
 import DataW from "../database/DataW";
